e1c94bc19a455612f78ebb4ea87039412e3612ff
Gabriel Calle Work <gabriel.calle92@gmail.com>
2022-12-05 15:02:46 +0100

    express-session added, app.js configured

--- app/app.js
+++ app/app.js
@@ -1,13 +1,34 @@
 const port = 3000;
 var express = require("express"),
+  mongoose = require('mongoose'),
   app = express(),
   server = require("http").createServer(app),
-  path = require("path");
+  path = require("path"),
+  session = require('express-session');
 
+// Server starting at port 3000
 server.listen(port, (err, res) => {
   if (err) console.log(`ERROR: Connecting APP ${err}`);
   else console.log(`Server is running on port ${port}`);
 });
+
+// Connection to MongoDB
+mongoose.connect(
+  `mongodb://root:pass12345@mongodb:27017/sesiones?authSource=admin`,
+  { useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true },
+  (err, res) => {
+    if (err) console.log(`ERROR: connecting to Database.  ${err}`);
+    else console.log(`Database Online`);
+  }
+);
+
+//session middleware
+app.use(session({
+  secret: "gabrielitoelmejor1234567890",
+  cookie: { maxAge: 1000*60*60, secure: false },
+  resave: false,
+  saveUninitialized: true
+}));
 
 // Import routes of our app
 var routes = require("./routes/main");
@@ -16,7 +37,7 @@ var routes = require("./routes/main");
 app.set("views", path.join(__dirname,"views"));
 app.set("view engine", "pug");
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Define routes using URL path
